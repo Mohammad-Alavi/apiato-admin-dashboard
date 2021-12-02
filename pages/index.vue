0272d58f8e646eb36a4703b4983532bb31b17ba7
zahir dezhbord <zahirdezhbord@gmail.com>
2021-12-03 01:25:05 +0330

    initiate

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <div class="text-center">
+        <h1>
+          IKON App
+        </h1>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default {
+}
+</script>
