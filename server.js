6c2738583f11465fe8375ca48107e199481df4ff
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2022-02-06 15:27:13 +0330

    Revert "fix router history mode"
    This reverts commit 6067e794902fed7af309a085439b5bc026484b2b.

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-var history = require('connect-history-api-fallback')
-
 // optional: allow environment to specify port
 const port = process.env.PORT || 3000
 
@@ -9,6 +7,5 @@ const express = require('express')
 const app = express()
 // bind the request to an absolute path or relative to the CWD
 app.use(express.static('dist'))
-app.use(history())
 // start the server
 app.listen(port, () => console.log(`Listening on port ${port}`))
