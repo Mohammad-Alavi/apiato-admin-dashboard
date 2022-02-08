af484bfe3bd490fd6188be4b5465b222b19f6555
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2022-02-08 16:13:56 +0330

    change faq text fields to text areas
    (cherry picked from commit 2fa789b6e28dcdc5ce27fe27da31fd716765bb05)

--- src/modules/faq/components/IkonFaqDialog.vue
+++ src/modules/faq/components/IkonFaqDialog.vue
@@ -3,28 +3,26 @@
         <v-row>
             <v-col cols="12" sm="6">
                 <validation-provider v-slot="{ errors }" name="Question (EN)" rules="required|min:2|max:700">
-                    <v-text-field v-model="localItem.question_en" :error-messages="errors"
-                                  :label="$vuetify.lang.t('$vuetify.pages.faqs.questionEn')">
-                    </v-text-field>
+                    <v-textarea counter :rows="textAreaRows" v-model="localItem.question_en" :error-messages="errors"
+                                  :label="$vuetify.lang.t('$vuetify.pages.faqs.questionEn')"/>
                 </validation-provider>
             </v-col>
             <v-col cols="12" sm="6">
                 <validation-provider v-slot="{ errors }" name="Answer (EN)" rules="required|min:2|max:700">
-                    <v-text-field v-model="localItem.answer_en" :error-messages="errors"
-                                  :label="$vuetify.lang.t('$vuetify.pages.faqs.answerEn')"></v-text-field>
+                    <v-textarea counter :rows="textAreaRows" v-model="localItem.answer_en" :error-messages="errors"
+                                  :label="$vuetify.lang.t('$vuetify.pages.faqs.answerEn')"/>
                 </validation-provider>
             </v-col>
           <v-col cols="12" sm="6">
                 <validation-provider v-slot="{ errors }" name="Question (DE)" rules="required|min:2|max:700">
-                    <v-text-field v-model="localItem.question_de" :error-messages="errors"
-                                  :label="$vuetify.lang.t('$vuetify.pages.faqs.questionDe')">
-                    </v-text-field>
+                    <v-textarea counter :rows="textAreaRows" v-model="localItem.question_de" :error-messages="errors"
+                                  :label="$vuetify.lang.t('$vuetify.pages.faqs.questionDe')"/>
                 </validation-provider>
             </v-col>
           <v-col cols="12" sm="6">
                 <validation-provider v-slot="{ errors }" name="Answer (DE)" rules="required|min:2|max:700">
-                    <v-text-field v-model="localItem.answer_de" :error-messages="errors"
-                                  :label="$vuetify.lang.t('$vuetify.pages.faqs.answerDe')"></v-text-field>
+                    <v-textarea counter :rows="textAreaRows" v-model="localItem.answer_de" :error-messages="errors"
+                                  :label="$vuetify.lang.t('$vuetify.pages.faqs.answerDe')"/>
                 </validation-provider>
             </v-col>
         </v-row>
@@ -37,6 +35,11 @@ export default {
   props: {
     item: {
       required: true
+    }
+  },
+  data () {
+    return {
+      textAreaRows: 2
     }
   },
   computed: {
