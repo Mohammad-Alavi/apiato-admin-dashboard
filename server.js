d652ee49271064d550cebe73b19a1972c051342e
Sebastian Weckend <32101271+yalsicor@users.noreply.github.com>
2022-05-01 20:54:47 +0200

    chore: add nocache headers

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 // optional: allow environment to specify port
 const port = process.env.PORT || 3000
 
+// no cache headers
+const nocache = {
+	'Surrogate-Control': 'no-store',
+	'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+	'Pragma': 'no-cache',
+	'Expires': '0',
+}
+
 // wire up the module
 const express = require('express')
 // create server instance
@@ -12,16 +20,13 @@ app.use(express.static('dist', {
   lastModified: false,
   setHeaders: (res, path) => {
     if (path.endsWith('.html')) {
-      res.setHeader('Surrogate-Control', 'no-store');
-      res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-      res.setHeader('Pragma', 'no-cache');
-      res.setHeader('Expires', '0');
+      res.header(nocache);
     }
   },
 }))
 // catch-all route
 app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html')
+  res.sendFile(__dirname + '/dist/index.html', {headers: nocache, lastModified: false, etag: false})
 })
 // start the server
 app.listen(port, () => console.log(`Listening on port ${port}`))
