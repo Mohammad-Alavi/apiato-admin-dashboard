6e18ac2aa334eb0514971b116754b5d4bb3ebc30
Sebastian Weckend <32101271+yalsicor@users.noreply.github.com>
2022-05-01 20:41:19 +0200

    chore: disable browser caching

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ const express = require('express')
 // create server instance
 const app = express()
 // bind the request to an absolute path or relative to the CWD
-app.use(express.static('dist', { index: false}))
+app.use(express.static('dist', { 
+  index: false,
+  etag: false,
+  lastModified: false,
+  setHeaders: (res, path) => {
+    if (path.endsWith('.html')) {
+      res.setHeader('Surrogate-Control', 'no-store');
+      res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+      res.setHeader('Pragma', 'no-cache');
+      res.setHeader('Expires', '0');
+    }
+  },
+}))
 // catch-all route
 app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html')
