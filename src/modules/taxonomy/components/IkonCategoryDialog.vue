8def7ddac9c4efba21c59d89acfa5e1235b2a3a3
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2022-09-28 14:44:36 +0330

    feat(validation): update the validation to match the backend

--- src/modules/taxonomy/components/IkonCategoryDialog.vue
+++ src/modules/taxonomy/components/IkonCategoryDialog.vue
@@ -2,12 +2,12 @@
   <v-container>
     <v-row>
       <v-col cols="12" sm="6">
-        <validation-provider v-slot="{ errors }" name="Label (EN)" rules="required|min:2|max:20" vid="label_en">
+        <validation-provider v-slot="{ errors }" name="Label (EN)" rules="required|min:2|max:30" vid="label_en">
           <v-text-field v-model="localItem.label_en" :error-messages="errors" :label="$vuetify.lang.t('$vuetify.pages.taxonomies.pages.categories.labelEn')"/>
         </validation-provider>
       </v-col>
       <v-col cols="12" sm="6">
-        <validation-provider v-slot="{ errors }" name="Label (DE)" rules="required|min:2|max:20" vid="label_de">
+        <validation-provider v-slot="{ errors }" name="Label (DE)" rules="required|min:2|max:30" vid="label_de">
           <v-text-field v-model="localItem.label_de" :error-messages="errors" :label="$vuetify.lang.t('$vuetify.pages.taxonomies.pages.categories.labelDe')"/>
         </validation-provider>
       </v-col>
